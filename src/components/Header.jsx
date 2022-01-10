fb33b293be07aa4840c7df8fa18d9e160aab2d2e
Meng Ting Chien <mengtingchien@mengde-mbp.myfiosgateway.com>
2022-01-10 15:57:19 -0500

    delete userName in mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -73,8 +73,9 @@ class Header extends React.Component {
                                     src={this.props.currentUser.photos[0].value}
                                     alt=""
                                 />
-                                {this.props.currentUser.displayName ? (<div>{this.props.currentUser.displayName}</div>) : (<div>{this.props.currentUser.username}</div>)}
-
+                                <div className="userName">
+                                    {this.props.currentUser.displayName ? (<div>{this.props.currentUser.displayName}</div>) : (<div>{this.props.currentUser.username}</div>)}
+                                </div>
                                 <div className="profile">
                                     <div style={{ backgroundColor: 'rgba(0,0,0,0)', borderRadius: '10px', color: 'white' }} className="ui animated black big fade button" tabIndex="0" onClick={this.logout}>
                                         <div className="visible content"><i className="sign-out icon" /></div>
