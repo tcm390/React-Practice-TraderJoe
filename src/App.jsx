ac2d20c0ad82142b6731b0298da66d4162e01e63
Meng Ting Chien <mengtingchien@mengde-mbp.myfiosgateway.com>
2022-01-10 02:11:12 -0500

    add http

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ class App extends React.Component {
 
     // };
     const getUser = () => {
-      fetch("https://traderjoesapi-wacky-tiger-ir.mybluemix.net/auth/login/success", {
+      fetch("http://traderjoesapi-wacky-tiger-ir.mybluemix.net/auth/login/success", {
         method: "GET",
         withCredentials: true,
         credentials: 'include',
